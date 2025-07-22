de456eb17abda3d71e8cc35dd8019e010c69ccb5
D101028 <104710811+D101028@users.noreply.github.com>
2025-07-22 20:48:48 +0800

    update cmd logger

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import logging
 import os
+import sys
 import time
 
 from support.config import Config
 from support.mylog import IP_FILE, DIARY_FILE, ERROR_FILE
 from support.updater import Updater
+
+# 設定 logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# 設定 handler 讓日誌輸出到 stdout
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 def init_data():
     if not os.path.isdir(Config.LOG_FOLDER):
@@ -19,5 +31,5 @@ if __name__ == "__main__":
     init_data()
     time.sleep(5)
     updater = Updater()
-    logging.info(">> Updater Started <<")
+    logger.info(">> Updater Started <<")
     updater.run()
